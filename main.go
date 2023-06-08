7113f2051d9943854bff71b76d5f4a725380de9e
smritidahal653 <smritidahal@gmail.com>
2023-06-07 19:06:47 -0700

    with separate go routines

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"strconv"
 	"sync"
+	"syscall"
 	"time"
 
 	k8sExec "github.com/smritidahal653/benchmark/exec"
 	k8sDiscovery "github.com/smritidahal653/benchmark/k8s"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -27,49 +28,62 @@ func main() {
 
 	os.Setenv("NUM_PODS_TO_CREATE", "10")
 
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go func() {
-		createPods(clientset)
-		wg.Done()
-	}()
-
-	wg.Add(1)
-	go func() {
-		executeCommandInPod(clientset, config)
-		wg.Done()
-	}()
-
-	// wg.Add(1)
-	// go func() {
-	// 	deletePod(clientset)
-	// 	wg.Done()
-	// }()
-
-	wg.Wait()
-}
-
-// creates number of pods specified by env var NUM_PODS_TO_CREATE
-func createPods(clientset *kubernetes.Clientset) {
 	numPods, err := strconv.Atoi(os.Getenv("NUM_PODS_TO_CREATE"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for {
-		if createdPods := getPodList(clientset, "default"); len(createdPods) < numPods {
+	// Create a stop channel to gracefully terminate the program
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	// Register a signal handler to handle termination signals
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		// Wait for termination signal
+		<-signals
+		fmt.Println("Termination signal received. Stopping...")
+
+		// Send stop signal to the goroutines
+		close(stopCh)
+	}()
+
+	// Run the createPods goroutine
+	go createPods(clientset, numPods, stopCh)
+
+	// Wait for a short duration to allow pods to be created
+	time.Sleep(5 * time.Second)
+
+	// Run the executeCommands goroutine
+	go executeCommandInPod(clientset, config, numPods, stopCh)
+
+	// Wait indefinitely
+	select {}
+}
+
+// creates number of pods specified by env var NUM_PODS_TO_CREATE
+func createPods(clientset *kubernetes.Clientset, numPods int, stopCh <-chan struct{}) {
+	var wg sync.WaitGroup
+	for i := 1; i <= numPods; i++ {
+		wg.Add(1)
+
+		go func(podIndex int) {
+			defer wg.Done()
+
 			// Define the pod object
+			podName := fmt.Sprintf("pod-%d", podIndex)
 			pod := &corev1.Pod{
 				ObjectMeta: metav1.ObjectMeta{
-					GenerateName: "pod-",
-					Labels:       map[string]string{"for": "exec"},
+					Name:   podName,
+					Labels: map[string]string{"for": "exec"},
 				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
-							Name:  "example-container",
-							Image: "nginx:latest",
+							Name:  "my-container",
+							Image: "nginx:stable",
 						},
 					},
 					ServiceAccountName: "pod-executor",
@@ -82,17 +96,33 @@ func createPods(clientset *kubernetes.Clientset) {
 				log.Fatal(err)
 			}
 			log.Printf("created %s successfully", createdPod.Name)
-		}
-		time.Sleep(time.Millisecond * 500)
+		}(i)
+
+		//Wait for a short duration before creating the next pod
+		time.Sleep(1 * time.Second)
 	}
+
+	wg.Wait()
 }
 
 // Execs into a random running pod and runs ls command
-func executeCommandInPod(clientset *kubernetes.Clientset, config *rest.Config) {
-	for {
-		//at least one pod needs to be created first
-		if createdPods := getPodList(clientset, "default"); len(createdPods) > 0 {
-			podToExecute := randPod(createdPods)
+func executeCommandInPod(clientset *kubernetes.Clientset, config *rest.Config, numPods int, stopCh <-chan struct{}) {
+	var wg sync.WaitGroup
+
+	for i := 1; i <= numPods; i++ {
+		wg.Add(1)
+
+		go func(podIndex int) {
+			defer wg.Done()
+
+			podName := fmt.Sprintf("pod-%d", podIndex)
+			podToExecute := &corev1.Pod{}
+			// Wait until the pod is in the running state
+			//err := wait.PollImmediate(1*time.Second, 10*time.Second, func() (bool, error) {
+			podToExecute, err := clientset.CoreV1().Pods("default").Get(context.TODO(), podName, metav1.GetOptions{})
+			if err != nil {
+				log.Fatal(err)
+			}
 
 			//can only exec if the pod is ready
 			if podToExecute.Status.Phase == corev1.PodRunning {
@@ -101,7 +131,7 @@ func executeCommandInPod(clientset *kubernetes.Clientset, config *rest.Config) {
 					ClientSet:     clientset,
 					RestConfig:    config,
 					PodName:       podToExecute.Name,
-					ContainerName: podToExecute.Spec.Containers[0].Name,
+					ContainerName: "my-container",
 					Namespace:     podToExecute.Namespace,
 				}
 
@@ -114,49 +144,19 @@ func executeCommandInPod(clientset *kubernetes.Clientset, config *rest.Config) {
 				} else {
 					log.Print("Successfully executed commands for ", podToExecute.Name)
 				}
+
+				//Wait for a short duration before deleting the pod
+				time.Sleep(1 * time.Second)
+				//delete the pod
+				err = clientset.CoreV1().Pods("default").Delete(context.TODO(), podName, metav1.DeleteOptions{})
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("deleted %s successfully", podName)
 			}
-		}
-		time.Sleep(time.Millisecond * 500)
+
+		}(i)
+
+		wg.Wait()
 	}
-}
-
-// Deletes a random pod
-func deletePod(clientset *kubernetes.Clientset) {
-	for {
-		//at least one pod needs to be created first
-		if createdPods := getPodList(clientset, "default"); len(createdPods) > 0 {
-			podToDelete := randPod(createdPods)
-			//delete the pod
-			err := clientset.CoreV1().Pods("default").Delete(context.TODO(), podToDelete.Name, metav1.DeleteOptions{})
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("deleted %s successfully", podToDelete.Name)
-		}
-		time.Sleep(time.Millisecond * 500)
-	}
-}
-
-// Retrieves all pods in the default namespace
-func getPodList(clientset *kubernetes.Clientset, namespace string) []corev1.Pod {
-	podList := &corev1.PodList{}
-
-	req := clientset.CoreV1().RESTClient().Get().Resource("pods").Namespace(namespace)
-	data, err := req.DoRaw(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := json.Unmarshal(data, &podList); err != nil {
-		log.Fatal(err)
-	}
-
-	return podList.Items
-}
-
-func randPod(items []corev1.Pod) *corev1.Pod {
-	index := rand.Intn(len(items))
-
-	randPod := items[index]
-	return &randPod
 }
